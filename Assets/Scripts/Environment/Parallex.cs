87fdd70f2a07ed462bee4e073a093faf805391b2
MOHAMMAD RAZAVIMOGHADAM <43119498+maxmrm@users.noreply.github.com>
2020-04-18 21:52:50 +0430

    add parallex
    add background
    add sky
    add star
    add parallex

---
+++ Assets/Scripts/Environment/Parallex.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallex : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public float scrollSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Mathf.Repeat(Time.time * scrollSpeed, 1);
+        this.gameObject.GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", new Vector2(x, 0f));
+    }
+}
