b5970456fd1a11946d02052b5b1e1293f2dc3190
MOHAMMAD RAZAVIMOGHADAM <43119498+maxmrm@users.noreply.github.com>
2020-04-18 22:00:17 +0430

    Update Parallex2.cs

--- Assets/Scripts/Environment/Parallex2.cs
+++ Assets/Scripts/Environment/Parallex2.cs
@@ -9,16 +9,40 @@ public class Parallex2 : MonoBehaviour
     private float[] parallexScales;
     public float smoothing = 1f;
 
+    private Transform cam;
+    private Vector3 previousCamPos;
 
+    private void Awake()
+    {
+        cam = Camera.main.transform;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        previousCamPos = cam.position;
+
+        parallexScales = new float[backGrounds.Length];
+        for(int i =0; i<backGrounds.Length; i++)
+        {
+            parallexScales[i] = backGrounds[i].position.z * -1;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        for (int i = 0; i < backGrounds.Length; i++)
+        {
+            float parallex = (previousCamPos.x - cam.position.x) * parallexScales[i];
+
+            float backgroundTargetPosX = backGrounds[i].position.x + parallex;
+
+            Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, backGrounds[i].position.y, backGrounds[i].position.z);
+
+            backGrounds[i].position = Vector3.Lerp(backGrounds[i].position, backgroundTargetPos, smoothing * Time.deltaTime);
+
+        }
+
+        previousCamPos = cam.position;
     }
 }
