50bb04787226e4b0b46ef074c8e9447b523e33b1
MOHAMMAD RAZAVIMOGHADAM <43119498+maxmrm@users.noreply.github.com>
2020-04-28 03:04:47 +0430

    add new feature
    add falling rock
    add moving rock

---
+++ Assets/Scripts/Platform/FallingPlatform.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingPlatform : MonoBehaviour
+{
+
+    private Rigidbody2D _rigidbody2D;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //StartCoroutine(ResetJumpRoutine());
+    }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.tag == "Player")
+        {
+            StartCoroutine(ResetJumpRoutine());
+        }
+    }
+    IEnumerator ResetJumpRoutine()
+    {
+        yield return new WaitForSeconds(3.0f);
+        //_rigidbody2D.WakeUp();
+        this._rigidbody2D.bodyType = RigidbodyType2D.Dynamic;
+        this._rigidbody2D.gravityScale = 2.0f;
+
+    }
+}
---
+++ Assets/Scripts/Platform/MovingPlatform.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField]
+    protected Transform startPos, pos1, pos2;
+    [SerializeField]
+    protected float _speed;
+    Vector3 nextPos;
+
+    private Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextPos = startPos.position;
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position == pos1.position)
+        {
+            nextPos = pos2.position;
+        }
+        else if (transform.position == pos2.position)
+        {
+            nextPos = pos1.position;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, nextPos, _speed * Time.deltaTime);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(pos1.position, pos2.position);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("in ");
+            player.transform.parent = this.transform;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("out ");
+        player.transform.parent = null;
+    }
+}
